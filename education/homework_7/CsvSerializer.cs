887cbb62a4f9a4331e9c237ba2c05dc90edd34cf
ustinov_os <ystikoo@mail.ru>
2025-07-19 19:05:41 +0300

    homework_7

---
+++ education/homework_7/CsvSerializer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace education.homework_7
+{
+    public static class CsvSerializer
+    {
+        public static string Serialize<T>(T obj)
+        {
+            var type = typeof(T);
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+            var header = new List<string>();
+            var values = new List<string>();
+
+            foreach (var prop in properties)
+            {
+                header.Add(prop.Name);
+                values.Add(prop.GetValue(obj)?.ToString() ?? string.Empty);
+            }
+
+            foreach (var field in fields)
+            {
+                header.Add(field.Name);
+                values.Add(field.GetValue(obj)?.ToString() ?? string.Empty);
+            }
+
+            return string.Join(",", values);
+        }
+
+        public static T Deserialize<T>(string csv) where T : new()
+        {
+            var values = csv.Split(',');
+            var obj = new T();
+            var type = typeof(T);
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+            int index = 0;
+
+            foreach (var prop in properties)
+            {
+                if (index >= values.Length) break;
+                var val = Convert.ChangeType(values[index], prop.PropertyType);
+                prop.SetValue(obj, val);
+                index++;
+            }
+
+            foreach (var field in fields)
+            {
+                if (index >= values.Length) break;
+                var val = Convert.ChangeType(values[index], field.FieldType);
+                field.SetValue(obj, val);
+                index++;
+            }
+
+            return obj;
+        }
+    }
+}
---
+++ education/homework_7/Program.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace education.homework_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterations = 1000;
+
+            // Сериализуемый объект
+            var obj = F.Get();
+
+            // --- Тестирование собственного CSV сериализатора ---
+            var watch = Stopwatch.StartNew();
+
+            string csvResult = "";
+            for (int i = 0; i < iterations; i++)
+            {
+                csvResult = CsvSerializer.Serialize(obj);
+            }
+
+            watch.Stop();
+            long serializeTime = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("CSV строка: " + csvResult);
+
+            watch.Restart();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                var deserialized = CsvSerializer.Deserialize<F>(csvResult);
+            }
+
+            watch.Stop();
+            long deserializeTime = watch.ElapsedMilliseconds;
+
+            // --- Тестирование Newtonsoft.Json ---
+            watch.Restart();
+
+            string jsonResult = "";
+            for (int i = 0; i < iterations; i++)
+            {
+                jsonResult = JsonConvert.SerializeObject(obj);
+            }
+
+            watch.Stop();
+            long jsonSerializeTime = watch.ElapsedMilliseconds;
+
+            watch.Restart();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                var deserialized = JsonConvert.DeserializeObject<F>(jsonResult);
+            }
+
+            watch.Stop();
+            long jsonDeserializeTime = watch.ElapsedMilliseconds;
+
+            // --- Вывод результата ---
+            Console.WriteLine();
+            Console.WriteLine("РЕЗУЛЬТАТЫ ТЕСТИРОВАНИЯ:");
+            Console.WriteLine("Сериализуемый класс: class F { int i1, i2, i3, i4, i5; }");
+            Console.WriteLine($"Количество итераций: {iterations}");
+            Console.WriteLine("Код сериализации-десериализации: Рефлексия через GetProperties/GetFields");
+            Console.WriteLine();
+            Console.WriteLine("МОЙ РЕФЛЕКСИОННЫЙ CSV:");
+            Console.WriteLine($"Время на сериализацию = {serializeTime} мс");
+            Console.WriteLine($"Время на десериализацию = {deserializeTime} мс");
+            Console.WriteLine();
+            Console.WriteLine("STANDARDНЫЙ МЕХАНИЗМ (Newtonsoft.Json):");
+            Console.WriteLine($"Время на сериализацию = {jsonSerializeTime} мс");
+            Console.WriteLine($"Время на десериализацию = {jsonDeserializeTime} мс");
+        }
+    }
+}
